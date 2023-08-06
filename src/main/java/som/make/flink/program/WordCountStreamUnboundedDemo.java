a8886569fb2e78d5e083724b73a96faddd1d1665
chengcheng <shmily_think@126.com>
2023-08-06 22:15:04 +0800

    本地运行webui

--- src/main/java/som/make/flink/program/WordCountStreamUnboundedDemo.java
+++ src/main/java/som/make/flink/program/WordCountStreamUnboundedDemo.java
@@ -2,6 +2,7 @@ package som.make.flink.program;
 
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -14,20 +15,22 @@ import org.apache.flink.util.Collector;
 public class WordCountStreamUnboundedDemo {
 
     public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        // StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        try (StreamExecutionEnvironment environment = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration())) {
 
-        DataStreamSource<String> streamSource = environment.socketTextStream("localhost", 7777);
+            DataStreamSource<String> streamSource = environment.socketTextStream("localhost", 7777);
 
-        SingleOutputStreamOperator<Tuple2<String, Integer>> sum = streamSource.flatMap((String value, Collector<Tuple2<String, Integer>> out) -> {
-            String[] words = value.split(" ");
-            for (String word : words) {
-                out.collect(Tuple2.of(word, 1));
-            }
-        }).returns(Types.TUPLE(Types.STRING, Types.INT)).keyBy(value -> value.f0).sum(1);
+            SingleOutputStreamOperator<Tuple2<String, Integer>> sum = streamSource.flatMap((String value, Collector<Tuple2<String, Integer>> out) -> {
+                String[] words = value.split(" ");
+                for (String word : words) {
+                    out.collect(Tuple2.of(word, 1));
+                }
+            }).setParallelism(2).returns(Types.TUPLE(Types.STRING, Types.INT)).keyBy(value -> value.f0).sum(1);
 
-        sum.print();
+            sum.print();
 
-        environment.execute();
+            environment.execute();
+        }
     }
 
 }
